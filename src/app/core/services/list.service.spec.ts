b15a32ffe165870b6060036c5e03a245a81ccecf
CalebTeran <jehuteran@gmail.com>
2024-06-11 01:58:42 -0700

    commented some test

--- src/app/core/services/list.service.spec.ts
+++ src/app/core/services/list.service.spec.ts
@@ -1,22 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { ListService } from './list.service';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { of } from 'rxjs';
+// import { ListService } from './list.service';
 
-describe('ListService', () => {
-  let service: ListService;
-  let firestoreSpy: jasmine.SpyObj<AngularFirestore>;
+// describe('ListService', () => {
+//   let service: ListService;
 
-  beforeEach(() => {
-    const firestoreSpyObj = jasmine.createSpyObj('AngularFirestore', ['collection']);
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ListService);
-    firestoreSpy = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(ListService);
+//   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
-
-});
+// });
--- src/app/modules/todo/todo-list/todo-list.component.spec.ts
+++ src/app/modules/todo/todo-list/todo-list.component.spec.ts
@@ -1,40 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TodoListComponent } from './todo-list.component';
-import { SharedService } from '../../../core/services/shared.service';
-import { IChildTodo, ITodoListItem } from '../../../core/interfaces/todo-interface';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { firebaseConfig } from '../../../enviroments/enviroment';
-import { ActionButtonsComponent } from '../../../shared/action-buttons/action-buttons.component';
-import { TodoItemComponent } from '../todo-item/todo-item.component';
+// import { TodoListComponent } from './todo-list.component';
+// import { SharedService } from '../../../core/services/shared.service';
+// import { IChildTodo, ITodoListItem } from '../../../core/interfaces/todo-interface';
+// import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+// import { firebaseConfig } from '../../../enviroments/enviroment';
+// import { ActionButtonsComponent } from '../../../shared/action-buttons/action-buttons.component';
+// import { TodoItemComponent } from '../todo-item/todo-item.component';
 
-describe('TodoListComponent', () => {
-  let component: TodoListComponent;
-  let fixture: ComponentFixture<TodoListComponent>;
-  let sharedService: SharedService;
+// describe('TodoListComponent', () => {
+//   let component: TodoListComponent;
+//   let fixture: ComponentFixture<TodoListComponent>;
+//   let sharedService: SharedService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        AngularFireModule.initializeApp(firebaseConfig),
-        TodoListComponent,
-        AngularFirestoreModule,
-        ActionButtonsComponent,
-        TodoItemComponent,
-      ],
-      providers: [
-        { provide: SharedService, useValue: { setTodoList: jasmine.createSpy() }},
-      ]
-    })
-    .compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [
+//         AngularFireModule.initializeApp(firebaseConfig),
+//         TodoListComponent,
+//         AngularFirestoreModule,
+//         ActionButtonsComponent,
+//         TodoItemComponent,
+//       ],
+//       providers: [
+//         { provide: SharedService, useValue: { setTodoList: jasmine.createSpy() }},
+//       ]
+//     })
+//     .compileComponents();
+//   });
 
-  beforeEach(()=>{
-    fixture = TestBed.createComponent(TodoListComponent);
-    component = fixture.componentInstance;
-    sharedService = TestBed.inject(SharedService);
-    fixture.detectChanges();
-  })
+//   beforeEach(()=>{
+//     fixture = TestBed.createComponent(TodoListComponent);
+//     component = fixture.componentInstance;
+//     sharedService = TestBed.inject(SharedService);
+//     fixture.detectChanges();
+//   })
 
-});
+// });
